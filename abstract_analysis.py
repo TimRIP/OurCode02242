a2befff125881fd5bf8c0aaaac5df80891be64e3
TimRIP <161326131+TimRIP@users.noreply.github.com>
2025-10-20 20:57:07 +0200

    Now with an analyzer

---
+++ abstract_analysis.py
@@ -0,0 +1,28 @@
+import sys
+import re
+
+import logging
+import jpamb
+
+from abstract import analyze_method_no_inputs
+
+# Always present
+methodid = jpamb.parse_methodid(sys.argv[1])
+
+classname, methodname, args = re.match(r"(.*)\.(.*):(.*)", sys.argv[1]).groups()
+
+logging.error(args)
+# Try to get concrete inputs if present (interpret mode); else run with TOPs
+abstract_outcome = analyze_method_no_inputs(methodid)
+
+# Use abstract_outcome in your analyzer logic...
+print("Abstract Outcome: " + abstract_outcome)  # or fold into your scoring
+
+
+# For now, we just print some dummy results
+print(f"ok;50%")
+print(f"divide by zero;50%") 
+print(f"assertion error;50%")
+print(f"out of bounds;50%")
+print(f"null pointer;50%")
+print(f"*;50%")
